29997e148da4d4658a16c8316deaf751dd33aec5
blacksun1998 <laurens.m.vos@gmail.com>
2024-08-05 23:15:47 +0200

    stuff

---
+++ checkpaths.py
@@ -0,0 +1,14 @@
+#windows
+
+#mac
+
+import os
+import psutil
+
+# Get the system root directory
+system_root = os.path.abspath(os.sep)
+print("System Root Directory:", system_root)
+
+# Get all hard drive letters
+drive_letters = [disk.device for disk in psutil.disk_partitions()]
+print("Hard Drive Letters:", drive_letters)
